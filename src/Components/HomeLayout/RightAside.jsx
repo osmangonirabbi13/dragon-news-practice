f935f553a643c653cda22f76909b979e27f3a1eb
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-05-04 11:28:10 +0600

    sticky scroll behaviour implemented

--- src/Components/HomeLayout/RightAside.jsx
+++ src/Components/HomeLayout/RightAside.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SoicalLogin from "./SoicalLogin";
 import FindUs from "./FindUs";
+import Qzone from "./Qzone";
 
 const RightAside = () => {
   return (
     <div className="space-y-8">
       <SoicalLogin></SoicalLogin>
       <FindUs />
+      <Qzone />
     </div>
   );
 };
